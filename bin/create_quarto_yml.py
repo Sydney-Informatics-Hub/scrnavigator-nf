7a8e67ff560d98ef6613ff56f00a25af4a17d896
mgeaghan <michael.p.geaghan@gmail.com>
2026-01-22 10:34:45 +1100

    update quarto conf script to allow submenus, add qc cluster report

--- bin/create_quarto_yml.py
+++ bin/create_quarto_yml.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import yaml
 import os
-from copy import deepcopy
 
-VALID_QMD_FILES = {
-    'Home': 'index.qmd',
-    'QC Filtering': 'qc_filter.qmd',
+VALID_QMD_FILES_NESTED = {
+    'Quality Control': [
+        ('Filtering', 'qc_filter.qmd'),
+        ('Clustering', 'qc_cluster.qmd'),
+    ]
 }
 
 def configure_quarto(navbar_entries: list):
@@ -13,7 +14,7 @@ def configure_quarto(navbar_entries: list):
         'project': {
             'type': 'website',
             'output-dir': 'report',
-            'render': '*.qmd',
+            'render': ['*.qmd'],
         },
         'format': {
             'html': {
@@ -43,16 +44,31 @@ def construct_navbar(qmd_files: list = []):
         for f in qmd_files
     ]
 
-    navbar_list = []
-    i = 0
-    for title, f in VALID_QMD_FILES.items():
-        if f not in qmd_basenames:
+    # Start with the index page
+    navbar_list = [
+        {'text': 'Home', 'href': 'index.qmd'}
+    ]
+
+    # Iterate through valid nested QMD files and add them if they exist
+    i = 1
+    for menu in ['Quality Control', 'Integration', 'Annotation', 'Analysis']:
+        valid_qmd_files = VALID_QMD_FILES_NESTED.get(menu)
+        if valid_qmd_files is None:
             continue
-        navbar_list.append({
-            'text': f'{i}. {title}',
-            'href': f,
-        })
-        i += 1
+        navbar_nested_dict = {
+            'text': f'{i}. {menu}',
+            'menu': []
+        }
+        j = 1
+        for title, f in valid_qmd_files:
+            if f not in qmd_basenames:
+                continue
+            navbar_nested_dict['menu'].append({
+                'text': f'{j}. {title}',
+                'href': f,
+            })
+            j += 1
+        navbar_list.append(navbar_nested_dict)
 
     return navbar_list
 
@@ -67,7 +83,7 @@ if __name__ == '__main__':
 
     qmd_config = configure_quarto(navbar_entries=navbar_entries)
 
-    config_file = '_quarto.qmd'
+    config_file = '_quarto.yml'
 
     with open(config_file, 'w') as f:
         yaml.safe_dump(qmd_config, f)
