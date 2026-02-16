15ef474d3cfed63f69e29f21277e54f42a085362
mgeaghan <michael.p.geaghan@gmail.com>
2026-02-16 16:59:38 +1100

    WIP: add dockerfile for sc packages

---
+++ docker/R/install.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env -S Rscript --vanilla
+
+# Install all required packages
+install.packages(
+  c(
+    "tidyverse",
+    "Seurat",
+    "hdf5r",
+    "clustree",
+    "DT",
+    "ggplot2",
+    "ggrepel",
+    "WebGestaltR",
+    "BiocManager",
+    "remotes",
+    "htmltools"
+  ),
+  repos = "https://cran.csiro.au/"
+)
+remotes::install_github("chris-mcginnis-ucsf/DoubletFinder")
+remotes::install_github("immunogenomics/presto")
+remotes::install_github("bnprks/BPCells/r")
+BiocManager::install(
+  c(
+    "SingleCellExperiment",
+    "SingleR",
+    "celldex",
+    "glmGamPoi",
+    "scuttle",
+    "DESeq2",
+    "EnsDb.Hsapiens.v86"
+  ),
+  site_repository = "https://cran.csiro.au/",
+  ask = FALSE
+)
